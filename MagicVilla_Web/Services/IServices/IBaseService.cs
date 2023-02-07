dad80c8a6fba8a3f3a1675f407e680b9fdc9430a
Smokedout89 <smokedout8989@gmail.com>
2023-02-07 20:51:35 +0200

    Added BaseService and implemented SendAsync

---
+++ MagicVilla_Web/Services/IServices/IBaseService.cs
@@ -0,0 +1,10 @@
+﻿namespace MagicVilla_Web.Services.IServices;
+
+using Models;
+
+public interface IBaseService
+{
+    APIResponse responseModel { get; set; }
+
+    Task<T> SendAsync<T>(APIRequest apiRequest);
+}
